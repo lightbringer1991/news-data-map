6c8ca4c39d5a202ed3fc214911c45e1c38f06df2
Peter Plucinski <peter.plucinski@gmail.com>
2016-07-30 07:14:24 +0000

    Added more information to each popup

--- js/map.js
+++ js/map.js
@@ -11,23 +11,29 @@ $(document).ready(function() {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Initialize Firebase
+    /*
+     *
+     * Initialize Firebase
+     *
+     */
     var config = {
-                    apiKey: "AIzaSyDhv0JZOeI6qpcb5ecuxybtFep5i68id3k",
-                    authDomain: "newsdatamashup.firebaseapp.com",
-                    databaseURL: "https://newsdatamashup.firebaseio.com",
-                    storageBucket: "newsdatamashup.appspot.com",
-                };
+        apiKey: "AIzaSyD1Dcv20dh6k26Lpvt8cl2llfRQ3nh2Ib4",
+        authDomain: "news-9219d.firebaseapp.com",
+        databaseURL: "https://news-9219d.firebaseio.com",
+        storageBucket: "news-9219d.appspot.com",
+    };
 
     firebaseApp = firebase.initializeApp(config);
+    var newsItemsRef = firebase.database().ref().limitToLast(250);
 
-    var newsItemsRef = firebase.database().ref().limitToLast(1000);
-
+    /*
+     *
+     * Show News Items On Map
+     *
+     */
     newsItemsRef.on('child_added', function(snapshot) {
-
         var newsItem = snapshot.val();
         addMarker(newsItem);
-
     });
 
     function addMarker(newsItem) {
@@ -36,15 +42,15 @@ $(document).ready(function() {
         var lng = newsItem.Longitude;
         var image = newsItem["Primary image"];
 
-        if(lat != undefined && lng != undefined && image != "") {
+        if(lat != undefined && lng != undefined) {
             L.marker([lat, lng]).addTo(map)
-                .bindPopup( '<a href="' + newsItem.URL + '" target="_blank"><strong>' + newsItem.Title + '</strong></a>' +
-                            '<br><img src="' + image + '">'
+                .bindPopup( '<p>Story Title: <a href="' + newsItem.URL + '" target="_blank"><strong>' + newsItem.Title + '</strong></a></p>' +
+                            '<p>Date: ' + newsItem.Date + '</p>' +
+                            '<p>Place: ' + newsItem.Place + '</p>' +
+                            '<p>Station: ' + newsItem.Station + '</p>' +
+                            '<img class="img-responsive" src="' + image + '">'
 
             ).openPopup();
         }
-
     }
-
-
 });
