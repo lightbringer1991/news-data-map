fca0c8236df07c2a346c10410a84b6e8ad685d26
Peter Plucinski <peter.plucinski@gmail.com>
2016-07-30 06:47:08 +0000

    Added article links to popup and custom style

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
      * Initialize map
      *
      */
-    var map = L.map('map').setView([-37.560906, 143.828050], 9);
+    var map = L.map('map').setView([-37.560906, 143.828050], 5);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -19,18 +19,32 @@ $(document).ready(function() {
                     storageBucket: "newsdatamashup.appspot.com",
                 };
 
-    var firebaseApp = firebase.initializeApp(config);
+    firebaseApp = firebase.initializeApp(config);
 
-    var newsItems = firebase.database().ref('newsdatamashup');
+    var newsItemsRef = firebase.database().ref().limitToLast(1000);
 
-    newsItems.on('child_added', function(data) {
-        var data = data.val() || 'Anonymous';
-        console.log(data);
+    newsItemsRef.on('child_added', function(snapshot) {
+
+        var newsItem = snapshot.val();
+        addMarker(newsItem);
+
     });
 
-    newsItems.once('value').then(function(snapshot) {
-      var keywords = snapshot.val();
-      console.log(keywords);
-    });
+    function addMarker(newsItem) {
+
+        var lat = newsItem.Latitude;
+        var lng = newsItem.Longitude;
+        var image = newsItem["Primary image"];
+
+        if(lat != undefined && lng != undefined && image != "") {
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup( '<a href="' + newsItem.URL + '" target="_blank"><strong>' + newsItem.Title + '</strong></a>' +
+                            '<br><img src="' + image + '">'
+
+            ).openPopup();
+        }
+
+    }
+
 
 });
